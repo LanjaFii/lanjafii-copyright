837bb2542ea041b274d048c814e54a2506b8802c
Lanja Fii <base3fita@gmail.com>
2024-10-03 13:15:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple script to handle form submission (mock)
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the default form submission
+    alert('Merci pour votre message !');
+});
